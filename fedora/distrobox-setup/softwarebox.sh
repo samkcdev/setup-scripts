b55c30daf3046dfdccfd2f3caba7bd4d76ab7b43
samkcdev <samnp87@gmail.com>
2026-09-08 14:03:31 +0530

    updated softwarebox script

--- fedora/distrobox-setup/softwarebox.sh
+++ fedora/distrobox-setup/softwarebox.sh
@@ -4,3 +4,6 @@ sudo dnf upgrade -y
 
 # installing calibre
 sudo dnf install libglvnd-opengl xcb-util xcb-util-cursor python3-pyqt6 qt6-qtwebengine xdg-icons
+
+# install cmatrix
+sudo dnf install cmatrix
