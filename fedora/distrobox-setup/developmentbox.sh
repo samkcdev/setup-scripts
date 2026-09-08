49c81af3d68a0b316d1bc090b2d5c094a4814b98
samkcdev <samnp87@gmail.com>
2026-09-08 13:09:18 +0530

    added scripts to install distroboxes

---
+++ fedora/distrobox-setup/developmentbox.sh
@@ -0,0 +1,11 @@
+
+#installing vscode
+# check vscode is installed yes then ignore
+# else install
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+sudo dnf check-update && sudo dnf install code 
+
+
+
---
+++ fedora/distrobox-setup/softwarebox.sh
@@ -0,0 +1,2 @@
+# installing calibre
+sudo dnf install libglvnd-opengl xcb-util xcb-util-cursor python3-pyqt6 qt6-qtwebengine xdg-icons
