493a95f22fe2e4ad838e423e23f0e1c5ae25729a
samkcdev <samnp87@gmail.com>
2026-09-26 00:36:53 +0530

    updated distrobox setup script

--- fedora/distrobox-setup/developmentbox.sh
+++ fedora/distrobox-setup/developmentbox.sh
@@ -7,7 +7,7 @@ sudo dnf upgrade -y
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-sudo dnf check-update && sudo dnf install code 
+sudo dnf check-update && sudo dnf install code vim
 
 
 
--- fedora/distrobox-setup/softwarebox.sh
+++ fedora/distrobox-setup/softwarebox.sh
@@ -3,7 +3,11 @@
 sudo dnf upgrade -y
 
 # installing calibre
-sudo dnf install libglvnd-opengl xcb-util xcb-util-cursor python3-pyqt6 qt6-qtwebengine xdg-icons
+sudo dnf install -y libglvnd-opengl xcb-util xcb-util-cursor python3-pyqt6 qt6-qtwebengine xdg-utils cmatrix vim
 
-# install cmatrix
-sudo dnf install cmatrix
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+
+echo "installation done"
+
+
+
